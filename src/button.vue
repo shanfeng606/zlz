ee8f2e90baf1bd5b1a1f1817ed8b10356b08e9da
SYF <1275260388@qq.com>
2020-06-11 18:09:31 +0800

    icon跟iconPosition属性添加

--- src/button.vue
+++ src/button.vue
@@ -1,9 +1,27 @@
 <template>
-  <button class="g-button">按钮</button>
+  <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
+    <svg v-if="icon" class="icon">
+      <use :xlink:href="`#i-${icon}`"></use>
+    </svg>
+    <div class="content">
+     <slot/>
+    </div>
+  </button>
 </template>
 
 <script>
-export default {};
+export default {
+  props:{
+    icon:{},
+    iconPosition:{
+      type:String,
+      default:'left',
+      validator(value){
+        return !(value!=='left' && value!=='right');
+      }
+    }
+  }
+};
 </script>
 
 <style lang="scss">
@@ -14,6 +32,10 @@ export default {};
   border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
   background: var(--button-bg);
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
   &:hover {
     border-color: var(--border-color-hover);
   }
@@ -22,6 +44,13 @@ export default {};
   }
   &:focus {
     outline: none;
+  }
+  >.content{order:2}
+  >.icon{order:1;margin-right:.2rem ;}
+
+  &.icon-right{
+    >.content{order: 1;}
+    >.icon{order:2;margin-right: 0;margin-left: .2rem;}
   }
 }
 </style>
